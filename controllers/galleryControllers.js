2f3b648adbbe9cc969154d50eb555bd800c0a912
sebastian lozano <sebaslozano708@gmail.com>
2024-11-23 12:53:18 -0500

    Added PUT gallery endpoint for user to update a Memory information

--- controllers/galleryControllers.js
+++ controllers/galleryControllers.js
@@ -21,14 +21,61 @@ const showGallery = async (req, res, next) => {
 const postOneImage = async (req, res, next) => {
 
     const { filename } = req.file;
-    const { user_id } = req.body;
+    const { user_id, title, description } = req.body;
 
     console.log("req.file", req.file);
     console.log("user_id", user_id);
 
     try{
-        const [result] = await database.execute("INSERT INTO gallery (user_id, url_path) VALUES (?, ?)", [+user_id, filename]);
+        const [result] = await database.execute("INSERT INTO gallery (user_id, url_path, title, description) VALUES (?, ?, ?, ?)", [+user_id, filename, title, description]);
+        console.log("POST result: ", result);
         res.status(200).json({"message": "new photo added!"});
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({message: error.message || "Internal server error"});
+        next();
+    }
+}
+
+
+
+
+// PUT request
+const updateImage = async (req, res, next) => {
+
+    const { pictureID } = req.params;
+    const { title, description } = req.body;
+    const file  = req.file;
+
+    const toNullFileName = file?.filename === undefined ? null : file.filename;
+    const toNullTitle = title === undefined ? null : title;
+    const toNullDescription = description === undefined ? null : description;
+    
+    try{
+        let imageFileToDelete;
+
+        if(toNullFileName){
+            //Select image of the row that is gonna be updated in case user changed it for a different one
+            const [rows] = await database.execute("SELECT url_path FROM gallery WHERE id = ?", [pictureID]);
+            imageFileToDelete = rows[0]?.url_path;
+        }
+
+        const [result] = await database.execute(`
+            UPDATE gallery SET url_path = IFNULL(?, url_path), title = IFNULL(?, title), description = IFNULL(?, description) 
+            WHERE id = ?`, [toNullFileName, toNullTitle, toNullDescription, pictureID]);
+
+        if(result.affectedRows <= 0){
+            res.status(404).json({"error": `Product with id ${pictureID} was not found!`});
+            return next();
+        }
+
+        // If user changed picture, delete the previous one from the server
+        if(toNullFileName){
+            fs.unlinkSync(__dirname + `../../uploads/${imageFileToDelete}`);
+        }
+
+        res.status(200).json({"message": "Updated successfully!"})
     }
     catch(error){
         console.error(error);
@@ -49,7 +96,6 @@ const deleteOneImage = async (req, res, next) => {
         //select row to be deleted and get the image file name
         const [rows] = await database.execute("SELECT url_path FROM gallery WHERE id = ?", [pictureID]);
         const imageFileToDelete = rows[0]?.url_path;
-        console.log(rows[0]?.url_path);
 
         const [result] = await database.execute("DELETE FROM gallery WHERE id = ?", [pictureID]);
         console.log("result: ", result);
@@ -59,6 +105,7 @@ const deleteOneImage = async (req, res, next) => {
             return next();
         }
 
+        //once the row has been deleted from our DB, we delete the image from our server
         fs.unlinkSync(__dirname + `../../uploads/${imageFileToDelete}`);
 
         res.status(200).json({"message": "deleted successfully!"});
@@ -74,4 +121,4 @@ const deleteOneImage = async (req, res, next) => {
 
 
 
-module.exports = { showGallery, postOneImage, deleteOneImage }
+module.exports = { showGallery, postOneImage, deleteOneImage, updateImage }
--- routes/galleryRoutes.js
+++ routes/galleryRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const upload = require("../config/imageStorage.js");
-const { showGallery, postOneImage, deleteOneImage } = require("../controllers/galleryControllers.js");
+const { showGallery, postOneImage, deleteOneImage, updateImage } = require("../controllers/galleryControllers.js");
 
 
 const galleryRoute = express.Router();
@@ -10,7 +10,7 @@ const galleryRoute = express.Router();
 galleryRoute.get("/gallery", showGallery);
 galleryRoute.post("/gallery", upload.single("picture"), postOneImage);
 galleryRoute.delete("/gallery/:pictureID", deleteOneImage);
-
+galleryRoute.put("/gallery/:pictureID", upload.single("picture"), updateImage);
 
 
 module.exports = galleryRoute;
